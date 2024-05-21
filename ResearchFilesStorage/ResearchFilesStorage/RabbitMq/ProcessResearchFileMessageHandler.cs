3d0c5fde4658ffef53ba8cfd824e35e3f7525405
CostinGabriel <80429103+CostinGabriel@users.noreply.github.com>
2024-05-21 18:35:55 +0300

    feat: handle process file completed message

---
+++ ResearchFilesStorage/ResearchFilesStorage/RabbitMq/ProcessResearchFileMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using RabbitMq.Extensions;
+using RabbitMq.Messages;
+using ResearchFilesStorage.Domain.Repository;
+
+namespace ResearchFilesStorage.RabbitMq;
+
+public class ProcessResearchFileCompletedMessageHandler(
+    IServiceScopeFactory serviceScopeFactory,
+    ILogger<ProcessResearchFileCompletedMessageHandler> logger) : IRequestHandler<HandleMessage<ProcessResearchFileCompletedMessage>>
+{
+    public async Task Handle(HandleMessage<ProcessResearchFileCompletedMessage> request, CancellationToken cancellationToken)
+    {
+        var message = request.Message!;
+        logger.LogInformation("Research file with Id: {Id} process completed", message.Id);
+
+        using IServiceScope scope = serviceScopeFactory.CreateScope();
+        IResearchFileRepository researchFileRepository =
+            scope.ServiceProvider.GetRequiredService<IResearchFileRepository>();
+
+        await researchFileRepository.SetSavedOnDiskAsync(message.Id, message.SavedLocally);
+    }
+}
